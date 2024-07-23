2d58cfa6702a26f0b0a4bff664b1f19c9fd98642
ImperFecti <adilm8909@gmail.com>
2024-07-23 08:36:09 +0700

    fixing myth auth error when login using some function in navbar, fixing bootstrap js in template, adding dropdown in navbar

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -2,15 +2,23 @@
     <!-- Footer -->
     <footer class="text-center text-white" style="background-color: #0a4275;">
         <!-- Grid container -->
-        <div class="container p-4 pb-0">
+        <div class="container p-3 pb-3">
             <!-- Section: CTA -->
-            <section class="">
-                <p class="d-flex justify-content-center align-items-center">
-                    <span class="me-3">Register for free</span>
-                    <button data-mdb-ripple-init type="button" class="btn btn-outline-light btn-rounded">
-                        Sign up!
-                    </button>
-                </p>
+            <section class="cta-section">
+                <?php if (logged_in()) : ?>
+                    <a type="button" class="btn btn-outline-light btn-rounded" href="/logout" role="button">Logout</a>
+                <?php else : ?>
+                    <div class="cta-container">
+                        <div class="cta-left">
+                            <span class="me-3">Register for free</span>
+                            <a type="button" class="btn btn-outline-light btn-rounded" href="/register">Sign up!</a>
+                        </div>
+                        <div class="cta-right">
+                            <span class="me-3">Already Have a Account?</span>
+                            <a type="button" class="btn btn-outline-light btn-rounded" href="/login">Log In!</a>
+                        </div>
+                    </div>
+                <?php endif; ?>
             </section>
             <!-- Section: CTA -->
         </div>
--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -1,7 +1,7 @@
 <!-- Navbar -->
 <nav class="navbar sticky-top navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/"><img id="MDB-logo" src="https://info-ambon.com/wp-content/uploads/2019/07/LOGO-PLN.png" alt="PLN Logo" draggable="false" height="30" /></a>
+        <a class="navbar-brand" href="/"><img src="https://info-ambon.com/wp-content/uploads/2019/07/LOGO-PLN.png" alt="PLN Logo" draggable="false" height="30" /></a>
         WEBSITE BAYAR LISTRIK ONLINE
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -19,6 +19,29 @@
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="/tagihanlistrik">Tagihan Listrik</a>
+                </li>
+
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        <!-- icon imported from bootstrap -->
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1" />
+                        </svg>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+                        <?php if (logged_in()) : ?>
+                            <li><a class="dropdown-item" href="/profile">Profile</a></li>
+                            <li><a class="dropdown-item" href="/settings">Settings</a></li>
+                            <li><a class="dropdown-item" href="/notifications">Notifications</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="/logout">Logout</a></li>
+                        <?php else : ?>
+                            <li><a class="dropdown-item" href="/login">Login</a></li>
+                        <?php endif; ?>
+                    </ul>
                 </li>
             </ul>
         </div>
