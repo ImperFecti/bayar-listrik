a1d402d6006969f3734f4b1cb040e38d96e5b1a4
ImperFecti <adilm8909@gmail.com>
2024-07-25 17:48:25 +0700

    fixing admin's controller

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -3,13 +3,12 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-use Myth\Auth\Entities\User;
 
 class Admin extends BaseController
 {
     public function profileadmin()
     {
         $user = user();
-        return view('pages/admin/admin', ['username' => $user->username]);
+        return view('pages/admin/admin',);
     }
 }
