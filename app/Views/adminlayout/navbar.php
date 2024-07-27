fcdc75d263cb2fa966b3f38d9c57b71296bf3bf5
ImperFecti <adilm8909@gmail.com>
2024-07-27 19:32:40 +0700

    fixing minor issue

--- app/Views/adminlayout/navbar.php
+++ app/Views/adminlayout/navbar.php
@@ -9,7 +9,7 @@
             <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                        <i class="fas fa-user fa-fw"></i> <?= $result->username ?>
+                        <i class="fas fa-user fa-fw"></i>
                     </a>
                     <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                         <li><a class="dropdown-item" href="/logout">Logout</a></li>
